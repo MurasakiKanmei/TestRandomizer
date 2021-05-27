f57b22fdb8c3b8bf3cdb493f9cb98789c1326730
MurasakiKanmei <83978374+MurasakiKanmei@users.noreply.github.com>
2021-05-26 20:59:46 -0400

    Add files via upload

---
+++ RandomizedTest.java
@@ -0,0 +1,52 @@
+/*
+ * Date: 2021/05/26
+ * Names: Daniel Yuan, Nikhil Sachdev, Jayden Huynh, Emily Ma
+ * Teacher: Mr. Ho
+ * Task: Create an anti-cheat test randomizer program
+ */ 
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+class RandomizedTest {
+	
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		String anotherQuestion;
+		int[] testQuestions = null;
+	}
+	
+	// Calculator method
+	
+	public static void problemCalculator(String [] args) {
+		
+	}
+	
+	// Test randomizer method
+	
+	public static void randomizer(String[] args) {
+		
+	}
+	
+	// User interface graphics method	
+	
+	public static void userInterface(String[] args) {
+		
+	}
+	
+	// Teacher's input scanner
+	
+	public static void examInputs(String[] args) {
+		
+	}
+	
+	// File creator method
+	
+	public static void fileCreation(String[] args) {
+		
+	}
+	
+	
+	
+}
